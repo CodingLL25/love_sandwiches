254df1b4fe16820dbe71b42dd8d9de63f3f6d3df
CodingLL25 <6b45ofgn@students.codeinstitute.net>
2025-08-09 10:14:26 +0100

    Refactoring update worksheet functions to remove repitition

--- run.py
+++ run.py
@@ -54,14 +54,14 @@ def validate_data(values):
     return True
 
 
-def update_sales_worksheet(data):
-    """"
-    Update sales worksheet, add new rows with the list data provided
+def update_worksheet(data, worksheet):
     """
-    print("Updating sales worksheet....\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n")
+    Update worksheet, add new row with the list data provided
+    """
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet has been updated successfully.\n")
 
 
 def calculate_surplus(sales_row):
@@ -91,9 +91,9 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus(sales_data)
-    print(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
 
 
 print("Welcome to Love Sandwiches Data Automation")
